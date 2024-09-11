6d47b0885b9ab80ec1f2881cfb2bdd440a58b5d6
leovolving <leo@leoyockey.com>
2024-09-11 09:07:30 -0400

    Select component initial commit

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
+import { getDisplayedValue } from "./Select.helpers";
+
+const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const SelectElement = styled.select`
+  position: absolute;
+  opacity: 0;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+const SelectMask = styled.div`
+  display: flex;
+  gap: 24px;
+  align-items: center;
+  border-radius: 8px;
+  padding: 12px;
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.gray50};
+
+  ${SelectElement}:focus + & {
+    outline: auto;
+    outline-color: -webkit-focus-ring-color;
+  }
+
+  ${SelectElement}:hover + & {
+    color: ${COLORS.black};
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <SelectElement value={value} onChange={onChange}>
+        {children}
+      </SelectElement>
+      <SelectMask aria-hidden="true">{displayedValue} <Icon id="chevron-down" /></SelectMask>
+    </Wrapper>
   );
 };
 
